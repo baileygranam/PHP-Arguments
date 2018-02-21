c106217b5c5a12691931db9bfe451d1e99089ee8
Bailey Granam <bgranam@stetson.edu>
2018-02-20 21:47:00 -0500

    Added parsing for flags and singles.

--- src/Models/Utilities/ParseArgv.php
+++ src/Models/Utilities/ParseArgv.php
@@ -5,21 +5,83 @@ namespace Models\Utilities;
 
 class ParseArgv
 {
-	private $argsParsed;
     private $argsUnparsed;
+    private $flags = array();
+    private $singles = array();
+    private $doubles = array();
+    private $size = 0;
 
     public function __construct($args)
     {
         $this->argsUnparsed = $args;
-        $this->argsParsed = array(
-            "One" => "one",
-            "Two" => "two"
-        );
     }
 
     public function getParsed()
     {
-        return $this->argsParsed;
+        return $this->argsUnparsed;
+    }
+
+    public function getSingles()
+    {
+    	return $this->singles;
+    }
+
+    public function getFlags()
+    {
+    	return $this->flags;
+    }
+
+   
+    public function parseFlags()
+    {
+		for($i = 0; $i < count($this->argsUnparsed); $i++)
+		{
+			if(preg_match("/^-.{1}$/", $this->argsUnparsed[$i], $match))
+			{
+				if (preg_match("/^-/", $this->argsUnparsed[$i+1], $match) || !isset($this->argsUnparsed[$i+1])) 
+				{
+					array_push($this->flags, str_replace("-","",$this->argsUnparsed[$i]));
+				}
+			}
+		}
+    }
+
+    /**
+    * Function to parse singles from the user provided arguments.
+    */
+    public function parseSingles()
+    {
+    	/* Loop through the arguments array. */
+    	for($i = 0; $i < count($this->argsUnparsed); $i++)
+		{
+			/* Check to see if the value at argsUnparsed[$i] contains a single hypen at the start of the string. */
+			if(preg_match("/^-.{1}$/", $this->argsUnparsed[$i], $match))
+			{
+				/* Check to make sure the value at argsUnparsed[$i+1] does not contain a hypen or is null. */
+				if (!preg_match("/^-/", $this->argsUnparsed[$i+1], $match) || !isset($this->argsUnparsed[$i+1])) 
+				{
+					/* 
+					* If the value at argsUnparsed[$i+1] is a comma seperated list then we are going to explode 
+					* the string and place it into an array. Then for each value in the array we will put it into 
+					* our singles array for the specific argument.
+					*/
+					if (preg_match("/,/", $this->argsUnparsed[$i+1], $match)) 
+					{
+						$tempData = explode(",",$this->argsUnparsed[$i+1]);
+						
+						foreach ($tempData as $key => $val)
+						{
+							$this->singles[str_replace("-","",$this->argsUnparsed[$i])][$key] = $val;
+						}
+					}
+					else
+					{
+						$tempSingles = array(str_replace("-","",$this->argsUnparsed[$i])=>$this->argsUnparsed[$i+1]);
+						$this->singles = array_merge($this->singles,$tempSingles);
+					}
+				}
+			}
+		}
     }
 }
 
--- src/testArgs.php
+++ src/testArgs.php
@@ -6,19 +6,53 @@ require_once 'Models/Utilities/ParseArgv.php';
 /* Give alias to name of class. */
 use Models\Utilities\ParseArgv;
 
+/* Check to validate that arguments were provided. */
 if(!isset($_SERVER['argv']))
 {
 	print("<b>Error:</b> No Arguments!");
 	exit();
 }
 
+/* Create new ParseArgv object. */
 $parsed = new ParseArgv($_SERVER['argv']);
+
+/* Get the parsed arguments. */
+$parsed->parseArgs();
 $arguments = $parsed->getParsed();
 
 
-foreach ($arguments as $k => $v) {
-    print("$k=>$v\n");
+
+foreach ($arguments as $k => $v) 
+{
+    print("$k => $v\n");
 }
+
+// print("\nFLAGS\n");
+
+// foreach ($flags as $k => $v) {
+//     print("'$v'\n");
+// }
+
+// print("\nSINGLES");
+
+// foreach ($singles as $k => $v) 
+// {
+// 	print("\n'$k' => ");
+
+// 	if(is_array($v))
+// 	{
+// 		foreach($v as $j => $i)
+// 		{
+// 		    print("[$j] '$i', ");
+// 		}
+// 	}
+// 	else
+// 	{
+// 		print("'$v'");
+// 	}
+// }
+
+
 
 
 
